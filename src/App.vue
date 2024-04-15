a632f6e35a185dd70242c53fdfb5c605e25d32e8
danielbosa <danielbosa95@gmail.com>
2024-04-15 22:22:16 +0200

    Loading Component added

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,24 @@
 <template>
-  <HeaderComponent/>
-  <MainComponent/>
+  <LoaderComponent v-if="this.store.loading"/>
+  <div v-if="!this.store.loading">
+    <HeaderComponent/>
+    <MainComponent/>
+  </div>
 </template>
 
 <script>
 import {store} from './store.js';
 import axios from 'axios';
 import HeaderComponent from './components/HeaderComponent.vue';
-import MainComponent from './components/MainComponent.vue'
+import MainComponent from './components/MainComponent.vue';
+import LoaderComponent from './components/LoaderComponent.vue';
 
   export default {
     name: 'App',
     components:{
       HeaderComponent,
       MainComponent,
+      LoaderComponent
     },
     data(){
       return{
@@ -22,11 +27,17 @@ import MainComponent from './components/MainComponent.vue'
     },
     methods:{
       getCards(){
+        this.store.loading = true; 
         axios.get(this.store.apiUrl).then((res)=>{
           this.store.cards = res.data.data;
-          console.log(this.store.cards);
-          //this.store.total = res.data.data
-        })
+          
+        }).catch((error) =>{
+            // handle error
+           console.log(error);
+           this.store.error.message = error.message;
+        }).finally(() =>{
+          this.store.loading = false;
+        });
       }
     },
     created(){
---
+++ src/components/LoaderComponent.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="d-flex justify-content-center align-items-center">
+        <img src="/images/kuriboh.webp" alt="">
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'LoaderComponent',
+    }
+</script>
+
+<style lang="scss" scoped>
+div {
+    position: absolute;
+    top: 0;
+    left: 0;
+    bottom: 0;
+    right: 0;
+    background-color: rgba(0, 0, 0, 0.5);
+    font-size: 4rem;
+}
+</style>
