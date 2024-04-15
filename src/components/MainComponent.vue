238cbf22c0c4f7c3843ce053ee3c20b6c90dc114
danielbosa <danielbosa95@gmail.com>
2024-04-15 16:01:32 +0200

    Main component styled

--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -1,7 +1,15 @@
 <template>
-    <div>
+    <div id="main">
+        <div class="db-container">
+            <div class="py-4">
+             search bar
+            </div>
+            <div class="db-container db-card-wrapper p-5">
 
+            </div>
+        </div>
     </div>
+    
 </template>
 
 <script>
@@ -14,5 +22,18 @@
 </script>
 
 <style lang="scss" scoped>
+@use '/src/assets/styles/partials/variables' as *;
 
+    #main{
+        background-color: $bgcolored;
+    }
+
+    .db-container{
+        width: 90%;
+        margin: 0 auto;
+    }
+
+    .db-card-wrapper{
+        background-color: $bglight;
+    }
 </style>
